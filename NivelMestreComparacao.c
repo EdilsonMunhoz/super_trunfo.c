f269fdce2c8a044922b3c1e8bf816adaae8d703b
EdilsonMunhoz <edilsonmunhozf@gmail.com>
2025-05-02 01:01:16 +0000

    TRABALHO FINALIZADO

---
+++ NivelMestreComparacao.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+
+int populacao1 = 12325000;
+int populacao2 = 674800;
+float area1 = 152111;
+float area2 = 120025;
+float pib1 = 69928;
+float pib2 = 30050;
+int pontosturisticos1 = 50;
+int pontosturisticos2 = 30;
+float densidade1 = 810247;
+float densidade2 = 562224;
+float pibpercapita1 = 5672432;
+float pibpercapita2 = 4453291;
+float superpoder1 = 1398572449792.00;
+float superpoder2 = 601001230336.00;
+
+printf("Carta 1 é maior que carta 2: %d\n", populacao1 > populacao2);
+printf("Carta 1 é maior que carta 2: %d\n", area1 > area2);
+printf("Carta 1 é maior que carta 2: %d\n", pib1 > pib2);
+printf("Carta 1 é maior que carta 2: %d\n", pontosturisticos1 > pontosturisticos2);
+printf("Carta 1 é menor que carta 2: %d\n", densidade1 < densidade2);
+printf("Carta 1 é maior que carta 2: %d\n", pibpercapita1 > pibpercapita2);
+printf("Carta 1 é maior que carta 2: %d\n", superpoder1 > superpoder2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
